3e691c370e0c1dd23ca8279a1d07bce4b15b0cf2
ozkancondek <ozkncndek@gmail.com>
2022-10-21 13:09:56 +0200

    Home Icon Added

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -2,7 +2,7 @@
   <nav>
     <ul>
       <li>
-        <nuxt-link exact to="/">Home</nuxt-link>
+        <nuxt-link exact to="/">Home </nuxt-link>
       </li>
       <li>
         <nuxt-link exact to="/restaurants">Restaurants</nuxt-link>
